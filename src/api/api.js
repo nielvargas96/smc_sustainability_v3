46e9369d47ad524c15cd9bb874b948628f7d9b9e
nielvargas <niel.vargas@ph.c3-interactive.com>
2024-03-10 22:59:42 +0800

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,98 +1,98 @@
 // Navigation
 
 export const navItems = [
-  // {
-  //   title: "Our Story",
-  //   href: "https://www.sanmiguel.com.ph/page/our-story",
-  //   subNav: [
-  //     {
-  //       title: "Our Company",
-  //       href: "https://www.sanmiguel.com.ph/page/our-company",
-  //       subNav2: [
-  //         {
-  //           title: "Our Values",
-  //           href: "https://www.sanmiguel.com.ph/page/our-values"
-  //         },
-  //         {
-  //           title: "Our Strategy",
-  //           href: "https://www.sanmiguel.com.ph/page/strategy"
-  //         },
-  //         {
-  //           title: "Our History",
-  //           href: "https://www.sanmiguel.com.ph/page/our-history"
-  //         },
-  //         {
-  //           title: "Corporate Social Responsibility",
-  //           href: "https://www.sanmiguel.com.ph/page/corporate-social-responsibilities",
-  //           subNav3: [
-  //             {
-  //               title: "Environmental and Other Programs",
-  //               href: "https://www.sanmiguel.com.ph/page/environmental-and-other-programs"
-  //             },
-  //             {
-  //               title: "World We Want",
-  //               href: "https://worldwewant.ph/"
-  //             }
-  //           ]
-  //         },
-  //         {
-  //           title: "Our Vision and Core Purpose",
-  //           href: "https://www.sanmiguel.com.ph/page/our-mission-and-vision"
-  //         },
-  //         {
-  //           title: "Board of Directors",
-  //           href: "https://www.sanmiguel.com.ph/page/board-of-directors"
-  //         },
-  //         {
-  //           title: "Company Officers",
-  //           href: "https://www.sanmiguel.com.ph/page/company-officers"
-  //         },
-  //         {
-  //           title: "Organizational Chart",
-  //           href: "https://www.sanmiguel.com.ph/page/organizational-chart"
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       title: "Our Businesses",
-  //       href: "https://www.sanmiguel.com.ph/page/our-businesses",
-  //       subNav2: [
-  //         {
-  //           title: "Food and Beverage",
-  //           href: "https://www.sanmiguel.com.ph/page/food-and-beverages"
-  //         },
-  //         {
-  //           title: "Oil Refining & Marketing",
-  //           href: "https://www.sanmiguel.com.ph/page/oil-refining-marketing"
-  //         },
-  //         {
-  //           title: "Cement",
-  //           href: "https://www.sanmiguel.com.ph/page/cement-business"
-  //         },
-  //         {
-  //           title: "Packaging",
-  //           href: "https://www.sanmiguel.com.ph/page/san-miguel-yamamura-packaging-corp"
-  //         },
-  //         {
-  //           title: "Properties",
-  //           href: "https://www.sanmiguel.com.ph/page/san-miguel-properties-inc"
-  //         },
-  //         {
-  //           title: "Power & Energy",
-  //           href: "https://www.sanmiguel.com.ph/page/power-energy"
-  //         },
-  //         {
-  //           title: "Infrastructure",
-  //           href: "https://www.sanmiguel.com.ph/page/infrastructure"
-  //         },
-  //         {
-  //           title: "Other Businesses",
-  //           href: "https://www.sanmiguel.com.ph/page/other-businesses"
-  //         }
-  //       ]
-  //     }
-  //   ]
-  // },
+  {
+    title: "Our Story",
+    href: "https://www.sanmiguel.com.ph/page/our-story",
+    subNav: [
+      {
+        title: "Our Company",
+        href: "https://www.sanmiguel.com.ph/page/our-company",
+        subNav2: [
+          {
+            title: "Our Values",
+            href: "https://www.sanmiguel.com.ph/page/our-values"
+          },
+          {
+            title: "Our Strategy",
+            href: "https://www.sanmiguel.com.ph/page/strategy"
+          },
+          {
+            title: "Our History",
+            href: "https://www.sanmiguel.com.ph/page/our-history"
+          },
+          {
+            title: "Corporate Social Responsibility",
+            href: "https://www.sanmiguel.com.ph/page/corporate-social-responsibilities",
+            subNav3: [
+              {
+                title: "Environmental and Other Programs",
+                href: "https://www.sanmiguel.com.ph/page/environmental-and-other-programs"
+              },
+              {
+                title: "World We Want",
+                href: "https://worldwewant.ph/"
+              }
+            ]
+          },
+          {
+            title: "Our Vision and Core Purpose",
+            href: "https://www.sanmiguel.com.ph/page/our-mission-and-vision"
+          },
+          {
+            title: "Board of Directors",
+            href: "https://www.sanmiguel.com.ph/page/board-of-directors"
+          },
+          {
+            title: "Company Officers",
+            href: "https://www.sanmiguel.com.ph/page/company-officers"
+          },
+          {
+            title: "Organizational Chart",
+            href: "https://www.sanmiguel.com.ph/page/organizational-chart"
+          }
+        ]
+      },
+      {
+        title: "Our Businesses",
+        href: "https://www.sanmiguel.com.ph/page/our-businesses",
+        subNav2: [
+          {
+            title: "Food and Beverage",
+            href: "https://www.sanmiguel.com.ph/page/food-and-beverages"
+          },
+          {
+            title: "Oil Refining & Marketing",
+            href: "https://www.sanmiguel.com.ph/page/oil-refining-marketing"
+          },
+          {
+            title: "Cement",
+            href: "https://www.sanmiguel.com.ph/page/cement-business"
+          },
+          {
+            title: "Packaging",
+            href: "https://www.sanmiguel.com.ph/page/san-miguel-yamamura-packaging-corp"
+          },
+          {
+            title: "Properties",
+            href: "https://www.sanmiguel.com.ph/page/san-miguel-properties-inc"
+          },
+          {
+            title: "Power & Energy",
+            href: "https://www.sanmiguel.com.ph/page/power-energy"
+          },
+          {
+            title: "Infrastructure",
+            href: "https://www.sanmiguel.com.ph/page/infrastructure"
+          },
+          {
+            title: "Other Businesses",
+            href: "https://www.sanmiguel.com.ph/page/other-businesses"
+          }
+        ]
+      }
+    ]
+  },
   {
     title: "Sustainability",
     href: "/",
@@ -225,148 +225,148 @@ export const navItems = [
       },
     ]
   },
-  // {
-  //   title: "Corporate",
-  //   href: "https://www.sanmiguel.com.ph/page/corporate",
-  //   subNav: [
-  //     {
-  //       title: "Corporate",
-  //       href: "https://www.sanmiguel.com.ph/page/corporate",
-  //       subNav2: [
-  //         {
-  //           title: "Manual of Corporate Governance",
-  //           href: "https://www.sanmiguel.com.ph/disclosures/manual-of-corporate-governance"
-  //         },
-  //         {
-  //           title: "Annual Corporate Governance Report",
-  //           href: "https://www.sanmiguel.com.ph/disclosures/annual-corporate-governance-report-acgr"
-  //         },
-  //         {
-  //           title: "Amended Articles of Incorporation and By-laws",
-  //           href: "https://www.sanmiguel.com.ph/disclosures/amended-articles-of-incorporation-and-bylaws"
-  //         },
-  //         {
-  //           title: "Board Committees",
-  //           href: "https://www.sanmiguel.com.ph/page/board-committees"
-  //         },
-  //         {
-  //           title: "Company's Policies",
-  //           href: "https://www.sanmiguel.com.ph/page/company-policy"
-  //         },
-  //         {
-  //           title: "Corporate Structure",
-  //           href: "https://www.sanmiguel.com.ph/page/corporate-structure"
-  //         },
-  //         {
-  //           title: "SMC Privacy Statement",
-  //           href: "https://www.sanmiguel.com.ph/page/smc-privacy-statement"
-  //         },
-  //         {
-  //           title: "Enterprise Risk Management",
-  //           href: "https://www.sanmiguel.com.ph/page/enterprise-risk-management"
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       title: "Investor Relations",
-  //       href: "https://www.sanmiguel.com.ph/page/corporate",
-  //       subNav2: [
-  //         {
-  //           title: "Shareholder Information",
-  //           href: "https://www.sanmiguel.com.ph/page/share-information",
-  //           subNav3: [
-  //             {
-  //               title: "President’s Message",
-  //               href: "https://www.sanmiguel.com.ph/page/presidents-message"
-  //             },
-  //             {
-  //               title: "Share Prices",
-  //               href: "https://www.sanmiguel.com.ph/shareprices"
-  //             },
-  //             {
-  //               title: "Dividend History",
-  //               href: "https://www.sanmiguel.com.ph/dividend-history"
-  //             },
-  //             {
-  //               title: "Analyst Coverage",
-  //               href: "https://www.sanmiguel.com.ph/page/analyst-coverage"
-  //             },
-  //             {
-  //               title: "Total Outstanding Shares",
-  //               href: "https://www.sanmiguel.com.ph/page/total-outstanding-shares"
-  //             },
-  //             {
-  //               title: "Top 20 Stockholders",
-  //               href: "https://www.sanmiguel.com.ph/page/top-20-stockholders"
-  //             },
-  //             {
-  //               title: "Exchange Where Listed",
-  //               href: "https://www.sanmiguel.com.ph/page/exchange-where-listed"
-  //             },
-  //             {
-  //               title: "Company Ownership Report",
-  //               href: "https://www.sanmiguel.com.ph/disclosures/public-ownership-report"
-  //             }
-  //           ]
-  //         },
-  //         {
-  //           title: "Company Disclosures",
-  //           href: "https://www.sanmiguel.com.ph/disclosures/all"
-  //         },
-  //         {
-  //           title: "Financial Performance",
-  //           href: "https://www.sanmiguel.com.ph/page/financial-performance",
-  //           subNav3: [
-  //             {
-  //               title: "Annual Reports",
-  //               href: "https://www.sanmiguel.com.ph/annual-reports"
-  //             },
-  //             {
-  //               title: "Financial Statements",
-  //               href: "https://www.sanmiguel.com.ph/financial-statements"
-  //             },
-  //             {
-  //               title: "IR Presentations",
-  //               href: "https://www.sanmiguel.com.ph/disclosures/ir-presentations"
-  //             },
-  //             {
-  //               title: "Financial Highlights",
-  //               href: "https://www.sanmiguel.com.ph/financial-highlights"
-  //             },
-  //             {
-  //               title: "Shelf Registration and Public Offering of Series 2 Preferred Shares’",
-  //               href: "https://www.sanmiguel.com.ph/disclosures/shelf-registration-and-public-offering-of-series-2-preferred-shares"
-  //             },
-  //             {
-  //               title: "Corporate Notes",
-  //               href: "https://www.sanmiguel.com.ph/disclosures/smc-peso-corporate-notes"
-  //             }
-  //           ]
-  //         },
-  //         {
-  //           title: "Investor Contact",
-  //           href: "https://www.sanmiguel.com.ph/page/investor-contact"
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       title: "Public Offering of Securities",
-  //       href: "https://www.sanmiguel.com.ph/disclosures/public-offering-of-securities"
-  //     },
-  //     {
-  //       title: "Walang Iwanan SMC's COVID-19 Efforts",
-  //       href: "https://www.sanmiguel.com.ph/stories/walang-iwanan-smcs-covid-19-efforts"
-  //     }
-  //   ]
-  // },
-  // {
-  //   title: "Careers",
-  //   href: "https://www.sanmiguel.com.ph/page/careers",
-  // },
-  // {
-  //   title: "News & Updates",
-  //   href: "https://www.sanmiguel.com.ph/news",
-  // },
+  {
+    title: "Corporate",
+    href: "https://www.sanmiguel.com.ph/page/corporate",
+    subNav: [
+      {
+        title: "Corporate",
+        href: "https://www.sanmiguel.com.ph/page/corporate",
+        subNav2: [
+          {
+            title: "Manual of Corporate Governance",
+            href: "https://www.sanmiguel.com.ph/disclosures/manual-of-corporate-governance"
+          },
+          {
+            title: "Annual Corporate Governance Report",
+            href: "https://www.sanmiguel.com.ph/disclosures/annual-corporate-governance-report-acgr"
+          },
+          {
+            title: "Amended Articles of Incorporation and By-laws",
+            href: "https://www.sanmiguel.com.ph/disclosures/amended-articles-of-incorporation-and-bylaws"
+          },
+          {
+            title: "Board Committees",
+            href: "https://www.sanmiguel.com.ph/page/board-committees"
+          },
+          {
+            title: "Company's Policies",
+            href: "https://www.sanmiguel.com.ph/page/company-policy"
+          },
+          {
+            title: "Corporate Structure",
+            href: "https://www.sanmiguel.com.ph/page/corporate-structure"
+          },
+          {
+            title: "SMC Privacy Statement",
+            href: "https://www.sanmiguel.com.ph/page/smc-privacy-statement"
+          },
+          {
+            title: "Enterprise Risk Management",
+            href: "https://www.sanmiguel.com.ph/page/enterprise-risk-management"
+          }
+        ]
+      },
+      {
+        title: "Investor Relations",
+        href: "https://www.sanmiguel.com.ph/page/corporate",
+        subNav2: [
+          {
+            title: "Shareholder Information",
+            href: "https://www.sanmiguel.com.ph/page/share-information",
+            subNav3: [
+              {
+                title: "President’s Message",
+                href: "https://www.sanmiguel.com.ph/page/presidents-message"
+              },
+              {
+                title: "Share Prices",
+                href: "https://www.sanmiguel.com.ph/shareprices"
+              },
+              {
+                title: "Dividend History",
+                href: "https://www.sanmiguel.com.ph/dividend-history"
+              },
+              {
+                title: "Analyst Coverage",
+                href: "https://www.sanmiguel.com.ph/page/analyst-coverage"
+              },
+              {
+                title: "Total Outstanding Shares",
+                href: "https://www.sanmiguel.com.ph/page/total-outstanding-shares"
+              },
+              {
+                title: "Top 20 Stockholders",
+                href: "https://www.sanmiguel.com.ph/page/top-20-stockholders"
+              },
+              {
+                title: "Exchange Where Listed",
+                href: "https://www.sanmiguel.com.ph/page/exchange-where-listed"
+              },
+              {
+                title: "Company Ownership Report",
+                href: "https://www.sanmiguel.com.ph/disclosures/public-ownership-report"
+              }
+            ]
+          },
+          {
+            title: "Company Disclosures",
+            href: "https://www.sanmiguel.com.ph/disclosures/all"
+          },
+          {
+            title: "Financial Performance",
+            href: "https://www.sanmiguel.com.ph/page/financial-performance",
+            subNav3: [
+              {
+                title: "Annual Reports",
+                href: "https://www.sanmiguel.com.ph/annual-reports"
+              },
+              {
+                title: "Financial Statements",
+                href: "https://www.sanmiguel.com.ph/financial-statements"
+              },
+              {
+                title: "IR Presentations",
+                href: "https://www.sanmiguel.com.ph/disclosures/ir-presentations"
+              },
+              {
+                title: "Financial Highlights",
+                href: "https://www.sanmiguel.com.ph/financial-highlights"
+              },
+              {
+                title: "Shelf Registration and Public Offering of Series 2 Preferred Shares’",
+                href: "https://www.sanmiguel.com.ph/disclosures/shelf-registration-and-public-offering-of-series-2-preferred-shares"
+              },
+              {
+                title: "Corporate Notes",
+                href: "https://www.sanmiguel.com.ph/disclosures/smc-peso-corporate-notes"
+              }
+            ]
+          },
+          {
+            title: "Investor Contact",
+            href: "https://www.sanmiguel.com.ph/page/investor-contact"
+          }
+        ]
+      },
+      {
+        title: "Public Offering of Securities",
+        href: "https://www.sanmiguel.com.ph/disclosures/public-offering-of-securities"
+      },
+      {
+        title: "Walang Iwanan SMC's COVID-19 Efforts",
+        href: "https://www.sanmiguel.com.ph/stories/walang-iwanan-smcs-covid-19-efforts"
+      }
+    ]
+  },
+  {
+    title: "Careers",
+    href: "https://www.sanmiguel.com.ph/page/careers",
+  },
+  {
+    title: "News & Updates",
+    href: "https://www.sanmiguel.com.ph/news",
+  },
 ];
 
 export const modalSustainabilityTargets = [
